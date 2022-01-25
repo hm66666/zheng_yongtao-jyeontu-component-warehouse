9d9903ba5a41a2763064495823f917008eb12efd
JYeontu <yongtaozheng0201@163.com>
2022-01-25 10:15:26 +0800

    台历组件

---
+++ JYeontuComponentWarehouse/packages/JDeskCalendar/index.js
@@ -0,0 +1,5 @@
+import JDeskCalendar from "./src/JDeskCalendar.vue";
+
+JDeskCalendar.install = Vue => Vue.component(JDeskCalendar.name, JDeskCalendar); //注册组件
+
+export default JDeskCalendar;
---
+++ JYeontuComponentWarehouse/packages/JDeskCalendar/src/JDeskCalendar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="desk-calender">
+        <div id="back" class="calender-panel"></div>
+        <div id="font" class="calender-panel"></div>
+        <div id="bottom" class="calender-panel"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JDeskCalendar"
+};
+</script>
+
+<style lang="scss" scoped>
+#desk-calender {
+    height: 200px;
+    .calender-panel {
+        position: absolute;
+        transform: rotateY(30deg);
+    }
+    #back {
+        background-color: aqua;
+        height: 150px;
+        width: 250px;
+        transform: rotateX(60deg);
+    }
+    #font {
+        background-color: aquamarine;
+        height: 150px;
+        width: 250px;
+        transform: rotateX(-60deg);
+    }
+    #bottom {
+        background-color: cadetblue;
+        height: 50px;
+        width: 250px;
+    }
+}
+</style>
