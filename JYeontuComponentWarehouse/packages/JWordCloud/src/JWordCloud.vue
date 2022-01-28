af32daa4403d90d0048c752aefbcc4d0adb8280a
JYeontu <yongtaozheng0201@163.com>
2022-01-28 17:47:11 +0800

    词云

--- JYeontuComponentWarehouse/packages/JWordCloud/src/JWordCloud.vue
+++ JYeontuComponentWarehouse/packages/JWordCloud/src/JWordCloud.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>111</div>
+    <div>
+        <div class="j-word-cloud">
+            <span
+                v-for="(item, index) in showTextList"
+                :key="index"
+                :id="'word-' + index"
+                :style="getStyle(item, index)"
+            >
+                {{ item.text }}
+            </span>
+        </div>
+        <div @click="init()">刷新</div>
+    </div>
 </template>
 
 <script>
@@ -13,36 +25,121 @@ export default {
                     { text: "测试", freq: 10 },
                     { text: "测试1", freq: 5 },
                     { text: "测试2", freq: 7 },
-                    { text: "测试3", freq: 2 }
+                    { text: "测试3", freq: 2 },
+                    { text: "测试5", freq: 3 },
+                    { text: "测试6", freq: 4 },
+                    { text: "测试7", freq: 5 },
+                    { text: "测试8", freq: 6 }
                 ];
             }
+        },
+        height: {
+            type: Number,
+            default: 200
+        },
+        width: {
+            type: Number,
+            default: 200
         },
         colorList: {
             type: Array,
             default: () => []
         },
+        baseSize: {
+            type: Number,
+            default: 1
+        },
         maxSize: {
             type: Number,
-            default: 500
+            default: 1500
         },
         minSize: {
             type: Number,
-            default: 100
+            default: 300
+        },
+        transformDeg: {
+            type: Array,
+            default: () => {
+                return [-45, 45];
+            }
         }
     },
     data() {
         return {
             maxFreq: 0,
             minFreq: 0,
-            showTextLise: []
+            showTextList: [],
+            pointList: []
         };
     },
     mounted() {
         this.init();
-        this.comShowtexList();
     },
     methods: {
         init() {
+            this.initData();
+            this.comShowtexList();
+            this.getFourPoints();
+        },
+        //组合样式
+        getStyle(item, index) {
+            const height = parseFloat(item.size) * 16;
+            const width = parseFloat(item.size) * 20 * item.text.length;
+            let res = "";
+            res += "font-size:" + item.size + ";";
+            res += "position: absolute;";
+            res +=
+                "top:" +
+                Math.max(0, Math.min(item.point.y, this.width - width)) +
+                "px;";
+            res +=
+                "left:" +
+                Math.max(0, Math.min(item.point.x, this.height - height)) +
+                "px;";
+            res += "color:" + item.color + ";";
+            // res += "transform:rotate(" + item.deg + "deg);";
+            return res;
+        },
+        //计算旋转后的坐标
+        getTransformPoint(x, y, deg) {
+            deg = parseFloat(deg);
+            let rx = (Math.cos(deg * Math.PI) / 180) * x,
+                ry = (Math.sin(deg * Math.PI) / 180) * x;
+            return { tx: x + rx, ty: y + ry };
+        },
+        //随机获取坐标
+        getRandomPoint() {
+            const width = this.width,
+                height = this.height,
+                pointList = this.pointList,
+                showTextList = this.showTextList;
+            const x = this.getRandomNum(0, width - 20),
+                y = this.getRandomNum(0, height - 20);
+            return { x: x, y: y };
+        },
+        //随机获取颜色
+        getRandomColor() {
+            let res = "rgb(";
+            res += this.getRandomNum(0, 255) + ",";
+            res += this.getRandomNum(0, 255) + ",";
+            res += this.getRandomNum(0, 255) + ")";
+            return res;
+        },
+        //随机获取角度
+        getRandomdeg() {
+            let res = "";
+            res += this.getRandomNum(
+                this.transformDeg[0],
+                this.transformDeg[1]
+            );
+            return res;
+        },
+        //获取随机数
+        getRandomNum(minN, maxN) {
+            return Math.floor(Math.random() * (maxN - minN + 1) + minN);
+        },
+        //初始化
+        initData() {
             const textList = this.textList;
             let maxF = textList[0].freq,
                 minF = textList[0].freq;
@@ -53,24 +150,75 @@ export default {
             this.maxFreq = maxF;
             this.minFreq = minF;
         },
+        //通过词频计算字体大小
         getSize(freq) {
-            const baseSize = this.minSize;
+            const baseSize = (this.maxSize - this.minSize) / 2 + this.minSize;
             const addSize =
                 ((this.maxSize - this.minSize) * (freq - this.minFreq)) /
                 (this.maxFreq - this.minFreq);
-            return baseSize + addSize;
+            return (
+                ((this.minSize + addSize) / baseSize) * this.baseSize + "rem"
+            );
         },
+        //获取四个顶点坐标
+        getFourPoints() {
+            this.$nextTick(() => {
+                let showTextList = this.showTextList;
+                for (let i = 0; i < showTextList.length; i++) {
+                    let id = "word-" + i;
+                    let dom = document.getElementById(id);
+                    let tl = { x: dom.offsetLeft, y: dom.offsetTop };
+                    let tr = {
+                        x: dom.offsetLeft + dom.offsetWidth,
+                        y: dom.offsetTop
+                    };
+                    let bl = {
+                        x: dom.offsetLeft,
+                        y: dom.offsetTop + dom.offsetHeight
+                    };
+                    let br = {
+                        x: dom.offsetLeft + dom.offsetWidth,
+                        y: dom.offsetTop
+                    };
+                    showTextList[i].fourPoints = {
+                        tl: tl,
+                        tr: tr,
+                        bl: bl,
+                        br: br
+                    };
+                }
+            });
+        },
+        //组装显示列表属性数据
         comShowtexList() {
-            let showTextLise = [];
+            let showTextList = [];
             this.textList.map(item => {
                 let temp = item;
                 temp.size = this.getSize(item.freq);
-                showTextLise.push(temp);
+                const point = this.getRandomPoint();
+                const color = this.getRandomColor();
+                const deg = this.getRandomdeg();
+                this.pointList.push(point);
+                temp.point = point;
+                temp.color = color;
+                temp.deg = deg;
+                showTextList.push(temp);
             });
-            this.showTextLise = showTextLise;
+            this.showTextList = showTextList;
         }
     }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.j-word-cloud {
+    border: 1px solid black;
+    position: relative;
+    height: 200px;
+    width: 200px;
+    span {
+        // border: 1px solid rgb(0, 0, 0);
+        transform-origin: 0 0;
+    }
+}
+</style>
