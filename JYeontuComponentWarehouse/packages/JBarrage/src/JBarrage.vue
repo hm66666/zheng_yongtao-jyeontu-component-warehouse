53e409dce52c25ea2e8b8d953a76ff1328bf4077
郑勇涛 <yongtaozheng0201@163.com>
2022-04-17 02:51:26 +0800

    弹幕组件

--- JYeontuComponentWarehouse/packages/JBarrage/src/JBarrage.vue
+++ JYeontuComponentWarehouse/packages/JBarrage/src/JBarrage.vue
@@ -1,6 +1,51 @@
 <template>
-    <div id="j-barrage-content" :style="{ '--width': width }">
-        <span class="text">{{ getColors() }}</span>
+    <div
+        id="j-barrage-content"
+        :style="{ '--width': width }"
+        @click="showToolsBox = false"
+    >
+        <div id="full-barrage-content" :style="{ '--width': width }"></div>
+        <div class="j-barrage-send-box">
+            <span
+                class="j-barrage-tools-box"
+                @click.stop="() => {}"
+                v-if="showToolsBox"
+            >
+                <div class="j-barrage-send-box-item">
+                    <span>颜色：</span
+                    ><input v-model="sendObj.color" type="color" />
+                </div>
+                <div class="j-barrage-send-box-item">
+                    <span>位置：</span>
+                    <template v-for="(pos, index) in position">
+                        <span :key="'pos-span-' + index">{{ pos }}</span>
+                        <input
+                            :key="'pos-input-' + index"
+                            name="position"
+                            type="radio"
+                            :value="pos"
+                            v-model="sendObj.position"
+                        />
+                    </template>
+                </div>
+            </span>
+            <span class="j-barrage-send-box-item input-box">
+                <span
+                    class="j-barrage-send-box-item-tools"
+                    @click.stop="showToolsBox = !showToolsBox"
+                    >A</span
+                >
+                <input
+                    class="j-barrage-send-box-item-input"
+                    v-model="sendObj.text"
+                    @focus="showToolsBox = false"
+                    @keydown.enter="sendBarrage"
+                />
+                <span class="j-barrage-send-box-item-btn" @click="sendBarrage"
+                    >发送</span
+                >
+            </span>
+        </div>
     </div>
 </template>
 
@@ -14,23 +59,73 @@ export default {
                 return [
                     {
                         text: "111",
-                        color: "",
-                        position: "" //top,center,bottom
+                        color: "red",
+                        position: "top" //top,center,bottom
                     },
                     {
                         text: "111",
-                        color: "",
-                        position: "" //top,center,bottom
+                        color: "red",
+                        position: "top" //top,center,bottom
                     },
                     {
                         text: "111",
-                        color: "",
-                        position: "" //top,center,bottom
+                        color: "red",
+                        position: "top" //top,center,bottom
                     },
                     {
                         text: "111",
-                        color: "",
-                        position: "" //top,center,bottom
+                        color: "red",
+                        position: "top" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "red",
+                        position: "top" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "blue",
+                        position: "center" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "blue",
+                        position: "center" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
+                    },
+                    {
+                        text: "111",
+                        color: "yellow",
+                        position: "bottom" //top,center,bottom
                     }
                 ];
             }
@@ -38,23 +133,49 @@ export default {
         full: {
             type: Boolean,
             default: true
+        },
+        time: {
+            type: Number,
+            default: 10
+        },
+        mask: {
+            type: Boolean,
+            default: false
+        },
+        repetition: {
+            type: Boolean,
+            default: false
+        },
+        startFrom: {
+            type: String,
+            default: "left"
         }
     },
     data() {
         return {
-            position: ["top", "bottom", "center"],
+            position: ["top", "center", "bottom", "random"],
             showBarrageDate: [],
             content: null,
             width: "1000px",
-            a: "blue"
+            a: "blue",
+            sendObj: {
+                text: "",
+                color: "#FFFFFF",
+                position: "random"
+            },
+            showToolsBox: false
         };
     },
     created() {},
     mounted() {
-        this.formatData();
+        this.formatDataList();
         this.generateBarrage();
     },
     methods: {
+        sendBarrage() {
+            const obj = this.formatData({ ...this.sendObj });
+            this.createBarrage(obj);
+        },
         getRandom(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min);
         },
@@ -68,58 +189,89 @@ export default {
         getPosition(position = "") {
             let content = this.content;
             let top = content.offsetTop;
-            let height = content.offsetHeight;
-            this.width = content.offsetWidth + "px";
-            if (this.full) {
-                height = window.screen.height;
-                this.width = window.screen.width + "px";
-                top = "0";
-                content.style = `--width:${this.width};`;
-            }
+            let height = content.offsetHeight * 0.9;
+            this.width =
+                content.offsetWidth +
+                (5 * content.offsetWidth) / this.time +
+                "px";
+            console.log("this.width", this.width);
             switch (position) {
                 case "top":
-                    return top + this.getRandom(0, height / 3);
+                    return this.getRandom(0, height / 3);
                 case "center":
-                    return top + this.getRandom(height / 3, (2 * height) / 3);
+                    return this.getRandom(height / 3, (2 * height) / 3);
                 case "bottom":
-                    return top + this.getRandom((2 * height) / 3, height);
+                    return this.getRandom((2 * height) / 3, height);
                 default:
-                    return top + this.getRandom(0, height);
+                    return this.getRandom(0, height);
             }
         },
-        formatData() {
+        formatData(item = {}) {
+            item.position = this.getPosition(item.position);
+            if (!item.color) item.color = this.getColors();
+            return item;
+        },
+        formatDataList() {
             if (this.full)
-                this.content = document.getElementsByTagName("body")[0];
+                this.content = document.getElementById("full-barrage-content");
             else this.content = document.getElementById("j-barrage-content");
             let showBarrageDate = [...this.barrageDate];
             showBarrageDate.map(item => {
-                item.position = this.getPosition(item.position);
-                if (!item.color) item.color = this.getColors();
+                item = this.formatData(item);
             });
             this.showBarrageDate = showBarrageDate;
         },
-        generateBarrage() {
+        destroyBarrage(dom = null) {
+            if (!dom) return;
             let content = this.content;
-            this.showBarrageDate.map(item => {
-                let span = document.createElement("span");
-                span.style.color = item.color;
-                span.innerHTML = item.text;
-                span.style.position = "relative";
-                if (this.full) span.style.position = "fixed";
-                span.style.top = item.position + "px";
-                span.style.animation = "move 10s";
-                span.classList.add("j-barrage-span", "text");
+            if (content.offsetLeft + content.offsetWidth < dom.offsetLeft)
+                content.removeChild(dom);
+            else {
                 setTimeout(() => {
-                    content.removeChild(span);
-                }, 10000);
-                content.appendChild(span);
+                    this.destroyBarrage(dom);
+                }, 1000);
+            }
+        },
+        createBarrage(item) {
+            let content = this.content;
+            let span = document.createElement("span");
+            span.style.color = item.color;
+            span.innerHTML = item.text;
+            span.style.position = "relative";
+            if (this.full) span.style.position = "fixed";
+            span.style.top = item.position + "px";
+            if (this.startFrom == "left") {
+                span.style.left = "0px";
+                span.style.animation = `moveLeft ${this.time}s linear`;
+            } else {
+                span.style.right = "0px";
+                span.style.animation = `moveRight ${this.time}s linear`;
+            }
+            span.classList.add("j-barrage-span", "text");
+            span.onmouseover = () => {
+                span.style.fontSize = "larger";
+                span.style.animationPlayState = "paused";
+            };
+            span.onmouseout = () => {
+                span.style.fontSize = "unset";
+                span.style.animationPlayState = "running";
+            };
+            content.appendChild(span);
+            this.destroyBarrage(span);
+        },
+        generateBarrage() {
+            this.showBarrageDate.map(item => {
+                let time = this.getRandom(1, 10);
+                setTimeout(() => {
+                    this.createBarrage(item);
+                }, time * 1000);
             });
         }
     }
 };
 </script>
 <style vars="{width}" lang="scss">
-@keyframes move {
+@keyframes moveLeft {
     from {
         left: 0px;
     }
@@ -127,15 +279,70 @@ export default {
         left: var(--width);
     }
 }
+@keyframes moveRight {
+    from {
+        right: 0px;
+    }
+    to {
+        right: var(--width);
+    }
+}
 </style>
 <style lang="scss" scoped>
+#full-barrage-content {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    pointer-events: none;
+}
 #j-barrage-content {
-    position: absolute;
-}
-.j-barrage-span {
-    animation: move 5s infinite;
-}
-.text {
-    position: relative;
+    background-color: #a46689;
+    width: 500px;
+    height: 100px;
+    // position: absolute;
+    // overflow: hidden;
+    .j-barrage-send-box {
+        .j-barrage-tools-box {
+            display: inline-block;
+            background-color: #c78d3b;
+            color: #fff;
+        }
+        .j-barrage-send-box-item {
+            padding: 0.5rem 0;
+        }
+        .input-box {
+            background-color: #f1f2f3;
+            height: 2rem;
+            padding: 0;
+            border-radius: 8%;
+            display: inline-block;
+            .j-barrage-send-box-item-tools {
+                text-decoration: underline;
+                padding: 0 0.5rem;
+                cursor: pointer;
+            }
+            .j-barrage-send-box-item-input {
+                height: 1.5rem;
+                line-height: 1.5rem;
+                outline: none;
+                border: none;
+                background-color: transparent;
+            }
+            .j-barrage-send-box-item-btn {
+                border-top-right-radius: 15%;
+                border-bottom-right-radius: 15%;
+                padding: 0.1rem;
+                cursor: pointer;
+                display: inline-block;
+                height: 2rem;
+                line-height: 2rem;
+                padding: 0 0.8rem;
+                background-color: #00aeec;
+            }
+        }
+    }
 }
 </style>
