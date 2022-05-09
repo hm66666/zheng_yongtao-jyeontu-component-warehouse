616c1624f760ea06a8c11caee4f99817177893fe
郑勇涛 <yongtaozheng0201@163.com>
2022-05-10 00:32:56 +0800

    '移动端触摸事件调试'

--- JYeontuComponentWarehouse/packages/JAppsLock/src/JAppsLock.vue
+++ JYeontuComponentWarehouse/packages/JAppsLock/src/JAppsLock.vue
@@ -178,7 +178,18 @@ export default {
                     (point.offsetLeft + point.offsetWidth + point.offsetLeft) /
                     2;
                 const r = point.offsetHeight / 2;
-                this.touchmoveTip = x + "," + y + "," + nx + "," + ny + "," + r;
+                this.touchmoveTip =
+                    x +
+                    "," +
+                    y +
+                    "," +
+                    nx.toFixed(2) +
+                    "," +
+                    ny.toFixed(2) +
+                    "," +
+                    r +
+                    "," +
+                    i;
                 if (Math.pow(x - nx, 2) + Math.pow(y - ny, 2) <= r * r) {
                     if (this.choosePoints.includes(i)) return;
                     this.choosePoints.push(i);
